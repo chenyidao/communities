899175cf64adf032ef9f49487ca6b53247ecd7c2
neu_cyd <979136761@qq.com>
2019-11-07 21:16:40 +0800

    first commit

---
+++ src/main/java/com/community/cyd/CydApplication.java
@@ -0,0 +1,13 @@
+package com.community.cyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CydApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CydApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/community/cyd/CydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.cyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CydApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
